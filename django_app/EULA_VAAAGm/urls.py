57321b4e97e649eaf74212cfc63e2384b2e19837
Malek BOUMEDINE <malek.boumedine@gmail.com>
2025-06-24 16:20:29 +0200

    correction chemin du fichier audio pour la lecture

--- django_app/EULA_VAAAGm/urls.py
+++ django_app/EULA_VAAAGm/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from ask_ur_16th_mommy.views import HomeView, GenerateAudioView
 
+from django.conf import settings  # ✅ nécessaire
+from django.conf.urls.static import static  # ✅ nécessaire
+
+
 app_name = 'eula_vaaagr'
 
 urlpatterns = [
@@ -26,6 +30,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
