2ae9a19cf0329d87cd4e361357a8afdd9afe254a
Malek BOUMEDINE <malek.boumedine@gmail.com>
2025-06-27 12:11:12 +0200

    correction du dockerfile de l'app django

--- django_entrypoint.sh
+++ django_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /EULA-vaaag-/django_app
+cd /django_app
 
 echo "Starting migrations..."
 python manage.py makemigrations
@@ -9,6 +9,5 @@ python manage.py migrate
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
 
-
 echo "Starting Gunicorn..."
-exec gunicorn EULA_VAAAGm.wsgi:application --bind 0.0.0.0:8001 --workers 8 --log-level debug --chdir /EULA-vaaag-/django_app
+exec gunicorn EULA_VAAAGm.wsgi:application --bind 0.0.0.0:8001 --workers 8 --log-level debug --chdir /django_app
